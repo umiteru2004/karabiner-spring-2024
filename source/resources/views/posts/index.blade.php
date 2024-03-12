d0f1a870cc4b8c096c9322a56e0b285d52759ca9
umiteru2004 <umiteru2004@gmail.com>
2024-03-12 21:09:41 +0900

    feat: ポストページに地図を表示するdivを追加しました。

--- source/resources/views/posts/index.blade.php
+++ source/resources/views/posts/index.blade.php
@@ -1,6 +1,8 @@
 @extends('welcome')
 
 @section('body')
+<div id="map" style="width: 100%; height: 100vh;">
+</div>
 <div class="container">
   <div class="row justify-content-center">
     <div class="col-md-12">
@@ -8,9 +10,9 @@
         <div class="card-header">投稿一覧</div>
 
         <div class="card-body">
-            <button type="button" class="btn btn-primary mb-3 d-block w-100" onclick="location.href='{{ route('posts.create') }}'">
-                新規投稿
-              </button>
+          <button type="button" class="btn btn-primary mb-3 d-block w-100" onclick="location.href='{{ route('posts.create') }}'">
+            新規投稿
+          </button>
           <div class="table-resopnsive">
             <table class="table table-striped">
               <thead>
@@ -24,7 +26,7 @@
                 @if(isset($posts))
                 @foreach ($posts as $post)
                 <tr>
-                    <td><a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a></td>
+                  <td><a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a></td>
                   <td>{{ $post->body }}</td>
                   <td>{{ $post->status }}</td>
                 </tr>

e7edec8cc403ee475e87b36ea2c9a8c8ce44f766
umiteru2004 <umiteru2004@gmail.com>
2024-03-13 10:57:31 +0900

    feat: 地図にマーカーを追加する機能を追加しました。

--- source/public/js/post.js
+++ source/public/js/post.js
@@ -1,4 +1,5 @@
 let map;
+let marker = null;
 
 async function initMap() {
     //@ts-ignore
@@ -7,7 +8,23 @@ async function initMap() {
     map = new Map(document.getElementById("map"), {
         center: { lat: -34.397, lng: 150.644 },
         zoom: 8,
+        mapId: "DEMO_MAP_ID",
     });
+
+    map.addListener("click", (e) => {
+        placeMarkerAndPanTo(e.latLng, map);
+    });
+}
+
+async function placeMarkerAndPanTo(latLng, map) {
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+    if (marker) marker.map = null;
+    marker = new AdvancedMarkerElement({
+        map,
+        position: latLng,
+    });
+    map.panTo(latLng);
 }
 
 initMap();
